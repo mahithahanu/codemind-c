b8b78d0d96a4fc957d9e28e5f89d865596666b6e
mahithahanu <119840694+mahithahanu@users.noreply.github.com>
2023-01-23 18:59:26 +0530

    #include<stdio.h> int main() {     int s,i;     scanf("%d",&s);     for(i=0;i<s;i++)     {         int x,y,m;         scanf("%d%d",&x,&y);         m=x*5+y*7;         printf("%d",m);         printf(" ");     } }

---
+++ Tour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int s,i;
+    scanf("%d",&s);
+    for(i=0;i<s;i++)
+    {
+        int x,y,m;
+        scanf("%d%d",&x,&y);
+        m=x*5+y*7;
+        printf("%d",m);
+        printf("
+");
+    }
+}
